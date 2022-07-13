97be115aa2795a06af943d1dfdb2c12f75529bc0
BenjaminAliriza <blaliriza@gmail.com>
2022-07-13 11:06:41 +0200

    Love Matt [  change -- pending ]

---
+++ src/test/java/stepdefinitions/MyStepdefs.java
@@ -0,0 +1,16 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+    @Given("I am in the login page of the Para Bank Application")
+    public void iAmInTheLoginPageOfTheParaBankApplication() {
+
+    }
+
+    @When("I enter valid credentials")
+    public void iEnterValidCredentials() {
+
+    }
+}
