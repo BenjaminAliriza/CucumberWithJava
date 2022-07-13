22ba4c742466f2145e7023d781b19645f83ab93e
Benjamin Aliriza <baliriza@dvt.co.za>
2022-07-13 13:52:37 +0200

    should be working, but it's not :')

--- src/test/java/stepdefinitions/MyStepdefs.java
+++ src/test/java/stepdefinitions/MyStepdefs.java
@@ -7,20 +7,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.time.Duration;
 
 public class MyStepdefs {
 
     private WebDriver driver;
+
     @Given("I am in the login page of the Para Bank Application")
     public void iAmInTheLoginPageOfTheParaBankApplication() {
-
         System.getProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         driver.get("https://parabank.parasoft.com/parabank/index.htm");
-
     }
     @When("I enter valid credentials")
     public void iEnterValidCredentials() {
@@ -34,4 +32,5 @@ public class MyStepdefs {
         driver.close();
         driver.quit();
     }
+
 }
