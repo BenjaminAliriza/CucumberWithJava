e22fd5ebb71c1f1bc0547c28fcc874154654df74
Benjamin Aliriza <baliriza@dvt.co.za>
2022-07-18 11:59:47 +0200

    IT'S ALIVE! AND IT WORKS!

--- src/test/java/runners/RunCucumberTests.java
+++ src/test/java/runners/RunCucumberTests.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(glue={"com.tau.steps"}, features = "src/test/resources", plugin = { "pretty", "html:target/site/cucumber-pretty",
+@CucumberOptions(glue={"stepdefinitions"}, features = "src/test/resources", plugin = { "pretty", "html:target/site/cucumber-pretty",
         "json:target/cucumber.json" })
 public class RunCucumberTests {
 }
